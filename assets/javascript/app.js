52531cc1808584370a382dc4a925ae213e43b59b
seamanhd <seamanhd@gmail.com>
2016-12-15 12:33:33 -0500

    javascript complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,3 @@
-
-
-
 //-----------------------------------------------------------------------------
 // GLOBAL VARIABLES
 //-----------------------------------------------------------------------------
@@ -14,130 +11,46 @@ var newTime;
 
 var myWatch;
 
-var qOne;
-var qTwo; 
-var qThree; 
-var qFour; 
-var qFive; 
-var qSix; 
-var qSeven;
+var q0;
+var q1;
+var q2; 
+var q3; 
+var q4; 
+var q5; 
+var q6; 
+
 
  var questions = []
 
-var createQuestions = function() {
-	qOne = {
-	question: "William Taft got stuck in the White House bathtub, and subsequently had a custom bathtub ordered.",
-	answer: true,
-}
-
-	questions.push(qOne);
-
-	qTwo = {
-	question: "John Quincy Adams was the 5th President of the United States.",
-	answer: false
-}
-
-	questions.push(qTwo);
-
-
-	qThree = {
-	question: "James Monroe was the shortest President.",
-	answer: false
-}
-	
-	questions.push(qThree)
-
-	 qFour = {
-	question: "William McKinley was the first President to campaign by telephone.",
-	answer: true
-}
-
-	questions.push(qFour)
-
-	qFive = {
-	question: "Ulysses S. Grant established Yellowstone as the first national park.",
-	answer: true
-}
-
-	questions.push(qFive)
-
-	qSix = {
-	question: "Martin Van Buren served the shortest Presidency, of only 28 days.",
-	answer: false
-}
-
-	questions.push(qSix)
-
-	qSeven = {
-	question: "Franklin D. Roosevelt is the only President to be elected 4 times.",
-	answer: true
-}
-
-	questions.push(qSeven)
-}
-
-
-createQuestions();
-console.log(questions);
-
-var printQuestions = function() {
-	for (var i = 0; i < questions.length; i++) {
-		$("#questions").append("<p>" + questions[i].question + " </p>" + "<form>" + "<input id='true' type='radio' name='answer'>" + "True" + "    " + "<input id='false' type='radio' name='answer'>" + "False" +   "</form>");
-		};
-	$("#questions").append("<button> Check my score! </button>");
-}
-
-printQuestions();
-
-window.onload = function() {
-
-	$("#display").text("02:00");
-	$("#start").click(stopwatch.start);
-
-
-}
-
-/*var checkQuestions = function(){
-	for (var i = 0; i < questions.length; i++) {
-		if questions[i].answer === 
-}
-*/
-
-
-	
+ var answers = []
 
  var stopwatch = {
 
- 	time: 3,
+ 	time: 20,
 
  	start: function() {
-
-    
     	myWatch = setInterval(stopwatch.count, 1000);
+  		},
 
-  	},
+  	stop: function() {
+    	clearInterval(myWatch);
+    	},
 
- 	
+  	reset: function() {
+
+    	stopwatch.time = 20;
+      $("#display").text("00.20");
+ 	 },
+
  	count: function() {
-
     	stopwatch.time--;
-
     	newTime = stopwatch.timeConverter(stopwatch.time);
-
     	$("#display").text(newTime);
 
     	if (newTime === "00:00") {
-
-    		$("#main").html("Time's Up!" + "<br>" + "Correct: " + correctCount + "<br>" + "Incorrect: " + wrongCount + "<br>" + "Total Answered: " + answeredCount + "<br>" + "<button> Play again </button>");
-    		$("#main").children("button").attr("id", "reset");
-    		$("#reset").on('click', function() {
-    			reset();
-
-    		});
+    		gameEnd();
     	}
   	},
-
-
 
  	timeConverter: function(t) {
 
@@ -161,60 +74,133 @@ window.onload = function() {
 
  };
 
- 
+ //--------------------------------------------------------------------------------
+ //FUNCTIONS
+ //--------------------------------------------------------------------------------
 
-//-----------------------------------------------------------------------------
-//FUNCTIONS
-//-----------------------------------------------------------------------------
+var createQuestions = function() {
+	q0 = {
+		question: "William Taft got stuck in the White House bathtub, and subsequently had a custom bathtub ordered.",
+		answer: "true",
+		}
 
+	questions.push(q0);
 
+	q1 = {
+		question: "John Quincy Adams was the 5th President of the United States.",
+		answer: "false",
+		}
 
+	questions.push(q1);
 
+	q2 = {
+		question: "James Monroe was the shortest President.",
+		answer: "false",
+		}
+	
+	questions.push(q2)
 
-var reset = function () {
-	// $("#display").text("02:00");
-	// $("#start").click(stopwatch.start);
-	// $("#main").empty();
-	console.log("Works")
+	 q3 = {
+		question: "William McKinley was the first President to campaign by telephone.",
+		answer: "true",
+		}
+
+	questions.push(q3)
+
+	q4 = {
+		question: "Ulysses S. Grant established Yellowstone as the first national park.",
+		answer: "true",
+		}
+
+	questions.push(q4)
+
+	q5 = {
+		question: "Martin Van Buren served the shortest Presidency, of only 28 days.",
+		answer: "false",
+		}
+
+	questions.push(q5)
+
+	q6 = {
+		question: "Franklin D. Roosevelt is the only President to be elected 4 times.",
+		answer: "true",
+		}
+
+	questions.push(q6)
 }
 
-//values on radio buttons of true and false, check whether matches answer 
-//.attr("value")
 
- // better way to do the ending of the countdown?
 
- /*var submit = function(){
-		for (var i = 0; i < questions.length; i++) {
-			if checkedvalue.questions[i] === questions[i].answer {
-				correctCount++;
-				answeredCount++;
-			}
-			if checked != answer {
-				wrongCount++;
-				answeredCount++;
-			}
+var printQuestions = function() {
+	for (var i = 0; i < questions.length; i++) {
+		$("#questions").append("<p>" + questions[i].question + " </p>" + "<form>" + "<input value='true' type='radio' name='q" + i + "'>" + "True" + "    " + "<input value='false' type='radio' name='q" + i + "'>" + "False" +   "</form>");
+		};
+	$("#questions").append("<button id='submit'> Check my score! </button>");
+}
+
+
+var getAnswers = function(){
+
+	for (var i = 0; i < questions.length; i++) {
+		
+		answers.push($("input[name='q" + i + "']:checked").val());
+		//console.log(answers);
+		if (questions[i].answer === answers[i]) {
+			correctCount++;
+			answeredCount++;	
 		}
-	}
-	*/
- // better way to distinguish answers - anonymous object? 
+		else if (answers[i] === undefined) {
+			console.log("not answered")
+		}
+		else if (questions[i].answer != answers[i]) {
+			wrongCount++;
+			answeredCount++;
+		}
+		
+	}		
+};
+
+var gameEnd = function () {
+			stopwatch.stop();
+    		stopwatch.reset();
+    		getAnswers();
+    		$("#display").html("Click start to play again!");
+    		$("#questions").html("Great job!" + "<br>" 
+    			+ "Correct: " + correctCount + "<br>" 
+    			+ "Incorrect: " + wrongCount + "<br>" 
+    			+ "Total Answered: " + answeredCount + "<br>");
+    		 
+}
 
 
-/* Pseudo-Code:
+//-----------------------------------------------------------------------------
+//RUN GAME
+//-----------------------------------------------------------------------------
 
-create timer to countdown 2 minutes
-create questions using html, give each a class of question and a unique id
-create T/F answers for each with radio buttons (how to do in html?)
-on click of question.true, trueCount++
-on click of question.false, falseCount++
-on click of question.true or question.false, answeredCount++
+window.onload = function() {
 
-when timer runs out, innerHTML (??) the true, false, answered
-on click of replay button, reset */
+	$("#display").text("Click start to begin the game!");
+	$("#start").click(function(){
+		stopwatch.start();
+		$("#questions").empty();
+		createQuestions();
+		printQuestions();
+	$("#submit").on('click', function() {
+		gameEnd();
 
-//logic for checking answers first?
-// create object for each question
-//create array of objects
-// use for loop to use jQuery to pick out parts of objects to write each question
-// look at question1, check for answers, then move 
-// .html the main div at the end, then reset runs loop to print Qs again
+	});
+	
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
 
